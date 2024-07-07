06dbf7000feb40e4b4d22cd14753d7f17e9def95
Ꮢαყααɳ <rayaannsn@gmail.com>
2024-07-07 20:28:50 +0530

    test: prettier-format using the script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-        <main className="min-h-screen grid place-content-center ">
+        <main className="grid min-h-screen place-content-center">
             <Button>Click me</Button>
         </main>
     );
